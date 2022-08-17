0624e8538e3cc262e9b08531c32343348cada528
ko1z-se-2015 <201402@astanait.edu.kz>
2022-08-17 16:07:18 +0600

    commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import '../styles/common.css'
 import sender from '../icons/profile.jpg'
 import cat from '../icons/cat.jpg'
+import like from '../icons/like.png'
+import comment from '../icons/comment.png'
+import share from '../icons/share.png'
 
 const Post = (props) => {
     return (
@@ -16,9 +19,9 @@ const Post = (props) => {
             <div className="content-text">{props.text ? props.text : "Заниматься жизни откуда упрекнуть некое избегал когда которое кто откуда порицающих жизни мог, немалое такие из-за предаваться только, возникает презирает поняли. Простейшим всю если картину иной из-за заниматься — превратное представление иной страдания тех превратное нас открывший боль тягостными, за как справедливости, некое счастливой никого но. Кто боль некоей великие превратное действительно нет, отвергает чтобы из-за никакого такого если открывший кто, назвал вами собой постигают, я, предпочел, какими возникает, как. Ни но, умеет, такого некоей презирает отвергает никого ни представление: презирает, лишь. Равно, возжаждал иной физическими говорил жизни физическими — воспользоваться, примером то равно."}</div>
             <img className="content-photo" src={props.photo ? props.photo : cat}/>
             <div className="like-comment-share">
-                <div className="like"><img /></div>
-                <div className="comment"></div>
-                <div className="share"></div>
+                <div className="lcs"><img className="icon-lcs" src={like}/></div>
+                <div className="lcs"><img className="icon-lcs" src={comment}/></div>
+                <div className="lcs"><img className="icon-lcs" src={share}/></div>
             </div>
         </div>
     );
